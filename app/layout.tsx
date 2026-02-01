03a63fd4418fb6988cbac6d358b6b6ca76de477c
manga-site <atsushi2002428@icloud.com>
2026-02-01 15:31:26 +0900

    add google search console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import TopTabs from "../components/TopTabs";
 export const metadata = {
   title: "コミック誌 情報まとめ",
   description: "発売日 / 懸賞 / 応募者全員サービス",
+   verification: {
+    google: "QvjZIVXrewSsk_JvwNtbP-cjhxF3Fa143mhYR-G8i7c", // ←ここに貼る
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -15,3 +18,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
